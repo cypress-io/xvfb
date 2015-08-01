d2c708075fd7d2a3be1a7ea82378a58256ec39cd
Brian Mann <brian@backbonerails.com>
2015-08-01 13:05:48 -0400

    remove legacy node stuff

--- index.js
+++ index.js
@@ -136,34 +136,3 @@ Xvfb.prototype = {
 }
 
 module.exports = Xvfb;
-
-
-if (require.main === module) {
-  var assert = require('assert');
-  var xvfb = new Xvfb({ displayNum: 88 });
-  xvfb.startSync();
-  console.error('started sync');
-  xvfb.stopSync();
-  console.error('stopped sync');
-  xvfb.start(function(err) {
-    assert.equal(err, null);
-    console.error('started async');
-    xvfb.stop(function(err) {
-      assert.equal(err, null);
-      console.error('stopped async');
-      xvfb.start(function(err) {
-        assert.equal(err, null);
-        console.error('started async');
-        xvfb.stopSync();
-        console.error('stopped sync');
-        xvfb.startSync();
-        console.error('started sync');
-        xvfb.stop(function(err) {
-          assert.equal(err, null);
-          console.error('stopped async');
-        });
-      });
-    });
-  });
-}
-
