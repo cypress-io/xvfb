7145ce1231213c052d4d38f5c44f8c754d0383bc
Brian Mann <brian@backbonerails.com>
2017-12-24 17:07:29 -0500

    chore: add tests

--- test/xvfb_spec.js
+++ test/xvfb_spec.js
@@ -3,6 +3,24 @@ const { expect } = require('chai')
 const Xvfb = require('../')
 
 describe('xvfb', function () {
+  context('onStderrData', function () {
+    it('accepts callback function', function () {
+      const cb = () => {}
+
+      const xvfb = new Xvfb({
+        onStderrData: cb,
+      })
+
+      expect(xvfb._onStderrData).to.eq(cb)
+    })
+
+    it('sets default function otherwise', function () {
+      const xvfb = new Xvfb()
+
+      expect(xvfb._onStderrData).to.be.a('function')
+    })
+  })
+
   context('issue: #1', function () {
     beforeEach(function () {
       this.xvfb = new Xvfb()
