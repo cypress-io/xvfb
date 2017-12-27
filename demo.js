1166bfaa12139a5882a3273a66137b477698f5b0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-12-27 10:59:23 -0500

    chore: test demo N procs in parallel several times

--- demo.js
+++ demo.js
@@ -2,24 +2,46 @@
 
 const Xvfb = require('.')
 const Promise = require('bluebird')
-const xvfb = Promise.promisifyAll(new Xvfb())
 
-xvfb
-.startAsync()
-.catch((err) => {
-  console.error('error starting XVFB')
-  console.error(err)
-  process.exit(1)
-})
-.then((xvfbProcess) => {
-  console.log('XVFB started', xvfbProcess.pid)
-  return xvfb.stopAsync()
-})
-.then(() => {
-  console.log('xvfb stopped')
-})
-.catch((err) => {
-  console.error('error stopping XVFB')
-  console.error(err)
-  process.exit(2)
-})
+function startStop () {
+  const xvfb = Promise.promisifyAll(new Xvfb())
+  return xvfb
+  .startAsync()
+  .catch((err) => {
+    console.error('error starting XVFB')
+    console.error(err)
+    process.exit(1)
+  })
+  .then((xvfbProcess) => {
+    console.log('XVFB started', xvfbProcess.pid)
+    return xvfb.stopAsync()
+  })
+  .then(() => {
+    console.log('xvfb stopped')
+  })
+  .catch((err) => {
+    console.error('error stopping XVFB')
+    console.error(err)
+    process.exit(2)
+  })
+}
+
+function testNprocs (N = 1) {
+  const procs = []
+  for (let k = 0; k < N; k += 1) {
+    procs.push(startStop())
+  }
+  return Promise.all(procs).then(() => {
+    console.log('all %d procs done', N)
+  })
+}
+
+Promise.mapSeries([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], () => testNprocs(1)).then(
+  () => {
+    console.log('all demo procs finished')
+  },
+  (err) => {
+    console.error('err', err)
+    process.exit(3)
+  }
+)
