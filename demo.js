f6f5cf7006cc95c46598496bf876cb977e984f97
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-12-27 12:21:43 -0500

    add demo to old code

---
+++ demo.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const Xvfb = require('.')
+const Promise = require('bluebird')
+// debug log messages from XVFB process
+const debugXvfb = require('debug')('xvfb-process')
+
+if (debugXvfb.enabled) {
+  console.log('XVFB process error stream enabled')
+}
+
+function startStop () {
+  const xvfb = Promise.promisifyAll(
+    new Xvfb({
+      onStderrData (data) {
+        if (debugXvfb.enabled) {
+          debugXvfb(data.toString())
+        }
+      },
+    })
+  )
+  return xvfb
+  .startAsync()
+  .catch((err) => {
+    console.error('error starting XVFB')
+    console.error(err)
+    process.exit(1)
+  })
+  .then((xvfbProcess) => {
+    console.log('XVFB started', xvfbProcess.pid)
+  })
+  .delay(100)
+  .then(() => {
+    return xvfb.stopAsync()
+  })
+  .then(() => {
+    console.log('xvfb stopped')
+  })
+  .catch((err) => {
+    console.error('error stopping XVFB')
+    console.error(err)
+    process.exit(2)
+  })
+}
+
+function testNprocs (N = 1) {
+  console.log('testing %d procs STARTS NOW', N)
+  const procs = []
+  for (let k = 0; k < N; k += 1) {
+    procs.push(startStop())
+  }
+  return Promise.all(procs).then(() => {
+    console.log('all %d procs done', N)
+    console.log('******')
+  })
+}
+
+Promise.mapSeries([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], () => testNprocs(1)).then(
+  () => {
+    console.log('all demo procs finished')
+  },
+  (err) => {
+    console.error('err', err)
+    process.exit(3)
+  }
+)
